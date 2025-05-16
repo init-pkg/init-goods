68266d0f56774aaa4c0715f44cdba86a66c8bf60
init-vadim <vadim@init.red>
2025-05-16 18:20:53 +0500

    fix: imports

--- src/next/index.ts
+++ src/next/index.ts
@@ -1,4 +1,4 @@
 export * from "./middlewares/middlewares";
-export * from "./middlewares/types";
 export * from "./middlewares/constant/setUrlMiddleware";
 export * from "./helpers/getSearchParams";
+export type * from "./middlewares/types";
