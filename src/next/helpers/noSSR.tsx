e1f2f2af026cd212beee18fa996fce0591bd204d
init-vadim <vadim@init.red>
2025-07-11 14:26:26 +0500

    remove: nossw

--- src/next/helpers/noSSR.tsx
+++ src/next/helpers/noSSR.tsx
@@ -1,36 +1,38 @@
-"use client";
+// "use client";
 
-import React, { ReactNode, useEffect, useState } from "react";
+// import { ReactNode, useEffect, useState } from "react";
 
-interface NoSSRElementProps {
-  children: ReactNode;
-  fallback?: ReactNode;
-}
+// interface NoSSRElementProps {
+//   children: ReactNode;
+//   fallback?: ReactNode;
+// }
 
-function useMounted() {
-  const [isClient, setIsClient] = useState(false);
+// function useMounted() {
+//   const [isClient, setIsClient] = useState(false);
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+//   useEffect(() => {
+//     setIsClient(true);
+//   }, []);
 
-  return {
-    isClient: typeof window !== "undefined",
-    isMounted: isClient,
-  };
-}
+//   return {
+//     isClient: typeof window !== "undefined",
+//     isMounted: isClient,
+//   };
+// }
 
-/**
- * Wraps a component to prevent it from rendering on the server side.
- * @param children - The component to wrap.
- * @param fallback - The fallback component to render while the component is loading.
- * @returns The wrapped component.
- */
-function NoSSR(props: NoSSRElementProps) {
-  const { children, fallback = null } = props;
-  const { isMounted, isClient } = useMounted();
-  if (!isMounted || !isClient) return fallback;
-  return <>{children}</>;
-}
+// /**
+//  * Wraps a component to prevent it from rendering on the server side.
+//  * @param children - The component to wrap.
+//  * @param fallback - The fallback component to render while the component is loading.
+//  * @returns The wrapped component.
+//  */
+// function NoSSR(props: NoSSRElementProps) {
+//   const { children, fallback = null } = props;
+//   const { isMounted, isClient } = useMounted();
+//   if (!isMounted || !isClient) return fallback;
+//   return <>{children}</>;
+// }
 
-export default NoSSR;
+// export default NoSSR;
+
+export {};
--- src/next/index.ts
+++ src/next/index.ts
@@ -3,4 +3,3 @@ export * from "./middlewares/constant/setUrlMiddleware";
 export * from "./helpers/getSearchParams";
 export type * from "./middlewares/types";
 export * from "./middlewares/utils/middlewareUtils";
-export { default as NoSSR } from "./helpers/noSSR";
